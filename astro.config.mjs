4a388a7804a662dbce46618ed534a540017f0344
LiquidFlame <liquidflame2600@gmail.com>
2025-12-25 19:58:11 -0600

    Fix font and favicon paths fot GitHub Pages base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://LiquidFlame.github.io',
-	base: '/liquidflame_blog',
+	base: '/liquidflame_blog/',
 	integrations: [mdx(), sitemap()],
 });
