a71943a585875592aa4c5f4c2ef65db8df3e33a7
LiquidFlame <liquidflame2600@gmail.com>
2025-12-25 18:06:04 -0600

    Configure site/base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://LiquidFlame.github.io',
+	base: '/liquidflame_blog',
 	integrations: [mdx(), sitemap()],
 });
