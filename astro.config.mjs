1f929aac59b9e26d2aea7f65986e303d42c75285
LiquidFlame <liquidflame2600@gmail.com>
2026-02-12 14:51:46 -0600

    Edited astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://liquidflame.github.io',
+	base: '/', // This must be '/' if your URL doesn't have a subfolder
 	integrations: [mdx(), sitemap()],
 });
