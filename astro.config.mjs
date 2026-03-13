cd1ad430d7955da36e516da7cb3dcd0ce2cc93c8
LiquidFlame <liquidflame2600@gmail.com>
2026-03-13 17:42:57 -0500

    Added search bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind'; // <-- 1. The new import
 
 export default defineConfig({
-	site: 'https://liquidflame.github.io',
-	base: '/',
-	integrations: [mdx(), sitemap()],
+    site: 'https://liquidflame.github.io',
+    base: '/',
+    integrations: [
+        mdx(), 
+        sitemap(), 
+        pagefind() // <-- 2. Added to the very end of the array
+    ],
 });
